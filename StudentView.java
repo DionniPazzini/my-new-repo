9b4beda1e0dc3987bc236757e520ee1ca738d366
DionniPazzini <101681065+DionniPazzini@users.noreply.github.com>
2022-03-21 11:50:40 -0600

    StudentView

---
+++ StudentView.java
@@ -0,0 +1,8 @@
+public class StudentView {
+
+    public void printStudentDetails(String studentName, String studentRollNo){
+    System.out.println("Alumn---- ");
+    System.out.println("Name: "+ studentName);
+    System.out.println("Roll no: " + studentRollNo);
+    }
+}
